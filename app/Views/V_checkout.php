29a3618461b69b4a471a736ef295b3b4ec60e79f
hasbyraihan <hasby608@gmail.com>
2024-05-17 08:43:03 +0700

    Front End

--- app/Views/V_checkout.php
+++ app/Views/V_checkout.php
@@ -7,25 +7,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <div class="container border mb-4 mt-4 rounded shadow bg-white">
+        <!-- menu-->
+        <nav class="d-md-flex p-4">
+            <div><h1>iCashier</h1></div>
+            <div class="ms-auto my-auto">
+                <ul class="list-inline m-0">
+                    <li class="list-inline-item mx-md-3"><a href="#Katalog" class="text-decoration-none text-dark fw-bold">Lihat Produk</a></li>
+                    <li class="list-inline-item mx-md-3"><a href="#Tentang" class="text-decoration-none text-dark fw-bold">Tentang saya</a></li>
+                </ul> 
+            </div>
+        </nav>
+        <!--banner-->
+        <div class="px-4 mb g-4 rounded-3">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuL8i2gbEi3PkgE6-rt8d5QsXLlwF3CjogYw&s" class="w-100">
+        </div>
 
-<div class="container">
-    <h1 class="mt-5 mb-4">Data Diri Pembeli</h1>
-    <form action="<?= base_url('/cart/complete_checkout'); ?>" method="post">
-        <div class="mb-3">
-            <label for="nama" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="nama" name="nama" required>
+        <div class="container">
+            <h1 class="mt-5 mb-4">Data Diri Pembeli</h1>
+            <form action="<?= base_url('/cart/complete_checkout'); ?>" method="post">
+                <div class="mb-3">
+                    <label for="nama" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="nama" name="nama" required>
+                </div>
+                <div class="mb-3">
+                    <label for="nomor_telepon" class="form-label">Nomor Telepon</label>
+                    <input type="text" class="form-control" id="nomor_telepon" name="nomor_telepon" required>
+                </div>
+                <div class="mb-3">
+                    <label for="alamat" class="form-label">Alamat</label>
+                    <textarea class="form-control" id="alamat" name="alamat" rows="3" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-success">Checkout</button>
+            </form>
         </div>
-        <div class="mb-3">
-            <label for="nomor_telepon" class="form-label">Nomor Telepon</label>
-            <input type="text" class="form-control" id="nomor_telepon" name="nomor_telepon" required>
-        </div>
-        <div class="mb-3">
-            <label for="alamat" class="form-label">Alamat</label>
-            <textarea class="form-control" id="alamat" name="alamat" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-success">Checkout</button>
-    </form>
-</div>
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
--- app/Views/V_success.php
+++ app/Views/V_success.php
@@ -8,12 +8,28 @@
 </head>
 <body>
 
-<div class="container">
-    <h1 class="mt-5 mb-4">Checkout Sukses</h1>
-    <p>Terima kasih telah berbelanja. Pesanan Anda telah berhasil diproses.</p>
-    <a href="<?= base_url('/'); ?>" class="btn btn-primary">Kembali ke Daftar Barang</a>
-</div>
+    <div class="container border mb-4 mt-4 rounded shadow bg-white">
+            <!-- menu-->
+            <nav class="d-md-flex p-4">
+                <div><h1>iCashier</h1></div>
+                <div class="ms-auto my-auto">
+                    <ul class="list-inline m-0">
+                        <li class="list-inline-item mx-md-3"><a href="#Katalog" class="text-decoration-none text-dark fw-bold">Lihat Produk</a></li>
+                        <li class="list-inline-item mx-md-3"><a href="#Tentang" class="text-decoration-none text-dark fw-bold">Tentang saya</a></li>
+                    </ul> 
+                </div>
+            </nav>
+            <!--banner-->
+            <div class="px-4 mb g-4 rounded-3">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuL8i2gbEi3PkgE6-rt8d5QsXLlwF3CjogYw&s" class="w-100">
+            </div>                                          
 
+        <div class="container">
+            <h1 class="mt-5 mb-4">Checkout Sukses</h1>
+            <p>Terima kasih telah berbelanja. Pesanan Anda telah berhasil diproses.</p>
+            <a href="<?= base_url('/'); ?>" class="btn btn-primary">Kembali ke Daftar Barang</a>
+        </div>
+    </div>  
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
